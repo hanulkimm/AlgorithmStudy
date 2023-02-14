e1aa3b0311fe5fff0eb6c201042978b0fa8faa92
hanulkimm <hanulkim2000@naver.com>
2023-02-14 09:01:58 +0900

    update week2study

--- Week 2/#3 단어뒤집기2 17413.py
+++ Week 2/#3 단어뒤집기2 17413.py
@@ -3,23 +3,23 @@ n = len(lst)
 ans = []
 i = 0
 while i < n:
-    if lst[i] == '<':
+    if lst[i] == '<': # '<'일 때 
         for j in range(i+1,n):
             if lst[j] == '>':
                 ans.append(lst[i:j+1])
                 i = j + 1
                 break
 
-    elif lst[i] == ' ':
+    elif lst[i] == ' ': # 공백일 때 
         ans.append([' '])
-        i += 1
-    else:
+        i += 1 
+    else: # 문자일 때
         for j in range(i+1, n):
-            if lst[j] == ' ' or lst[j] == '<' or lst[j] == '>':
+            if lst[j] == ' ' or lst[j] == '<' or lst[j] == '>': # 글자 제외한 것 만났을 때
                 ans.append(lst[i:j][::-1])
                 i = j
                 break
-            if j == n-1:
+            if j == n-1: # 끝까지 문자일 때
                 ans.append(lst[i:j+1][::-1])
                 i = n
                 break
