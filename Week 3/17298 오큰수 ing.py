124330f9e687e98129de3a3fd6d24cf11db60d22
hanulkimm <hanulkim2000@naver.com>
2023-02-21 15:33:24 +0900

    update code

--- Week 3/17298 오큰수 ing.py
+++ Week 3/17298 오큰수 ing.py
@@ -1,11 +1,12 @@
-import sys
-sys.stdin = open('input.txt', 'r')
+## 시간 초과 ##
+# 다른 방법 생각하기..
 
 n = int(input())
 lst = list(map(int, input().split()))
 ans = []
-flag = 'F'
+
 for i in range(n-1):
+    flag = 'F'
     for j in range(i+1, n):
         if lst[i] < lst[j]:
             ans.append(lst[j])
