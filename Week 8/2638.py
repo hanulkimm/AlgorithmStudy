c8163c891a187c88fe9e8f46094f460d4fbfbbdd
hanulkimm <hanulkim2000@naver.com>
2023-03-27 17:48:11 +0900

    update ongoing

--- Week 8/2638.py
+++ Week 8/2638.py
@@ -7,18 +7,18 @@ def cheese(arr):
             if arr[i][j]==1:
                 return True
 
-def bfs(i,j): # 둘러싸인 공간 판정...어떻게>>?
-    # tmp = 0
-    # for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-    #     for k in range(1,max(n,m)):
-    #         ni,nj = i+di*k, j+dj*k
-    #         if 0<=ni<n and 0<=nj<m and arr[ni][nj]==1:
-    #             tmp += 1
-    #             break
-    # if tmp==4:
-    #     return False
-    # else:
-    #     return True
+def bfs(arr):
+    si,sj = 0,0
+    q = deque()
+    q.append((si,sj))
+    arr[si][sj]=2
+    while q:
+        ci, cj = q.popleft()
+        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)): # 상하좌우
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<n and 0<=nj<m and arr[ni][nj]==0:
+                arr[ni][nj]=2
+                q.append((ni,nj))
 
 def melt(arr):
     tmp = [[1]*m for _ in range(n)]
@@ -28,14 +28,15 @@ def melt(arr):
                 num = 0
                 for di,dj in ((-1,0),(1,0),(0,-1),(0,1)): # 상하좌우
                     ni,nj = i+di, j+dj
-                    if 0<=ni<n and 0<=nj<m and arr[ni][nj]==0:
-                        if bfs(ni,nj):
-                            num += 1
+                    if 0<=ni<n and 0<=nj<m and arr[ni][nj]==2:
+                        num += 1
                 if num>=2:
                     tmp[i][j]=0
     for i in range(1, n - 1):
         for j in range(1, m - 1):
             if tmp[i][j]==0:
+                arr[i][j]=0
+            if arr[i][j]==2:
                 arr[i][j]=0
 
 
