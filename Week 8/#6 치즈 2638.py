88fa1388fa3eaaa907d96ec10e75c3673064d706
hanulkimm <hanulkim2000@naver.com>
2023-03-27 21:28:54 +0900

    add 2638 code

--- Week 8/#6 치즈 2638.py
+++ Week 8/#6 치즈 2638.py
@@ -1,13 +1,13 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 from collections import deque
-def cheese(arr):
+def cheese(arr): # 치즈 유무 판정
     for i in range(1, n-1):
         for j in range(1, m-1):
             if arr[i][j]==1:
                 return True
 
-def bfs(arr):
+def bfs(arr): # 치즈 범위 밖에 있는 빈 공간표시
     si,sj = 0,0
     q = deque()
     q.append((si,sj))
@@ -20,23 +20,23 @@ def bfs(arr):
                 arr[ni][nj]=2
                 q.append((ni,nj))
 
-def melt(arr):
+def melt(arr): # 치즈 녹이기
     tmp = [[1]*m for _ in range(n)]
-    for i in range(1, n-1):
+    for i in range(1, n-1): # 가장 자리는 없음
         for j in range(1,m-1):
             if arr[i][j]==1:
                 num = 0
                 for di,dj in ((-1,0),(1,0),(0,-1),(0,1)): # 상하좌우
                     ni,nj = i+di, j+dj
-                    if 0<=ni<n and 0<=nj<m and arr[ni][nj]==2:
+                    if 0<=ni<n and 0<=nj<m and arr[ni][nj]==2: # 전체 범위 내이고 치즈 범위 밖
                         num += 1
                 if num>=2:
                     tmp[i][j]=0
-    for i in range(1, n - 1):
-        for j in range(1, m - 1):
+    for i in range(n):
+        for j in range(m):
             if tmp[i][j]==0:
                 arr[i][j]=0
-            if arr[i][j]==2:
+            if arr[i][j]==2: # 다시 빈 공간으로 표시
                 arr[i][j]=0
 
 
@@ -44,6 +44,7 @@ n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 while cheese(arr):
+    bfs(arr)
     melt(arr)
     ans += 1
 print(ans)
