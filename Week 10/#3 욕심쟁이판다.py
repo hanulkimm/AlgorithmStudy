28c026b3f35aa51289d7083fe521c3bf2984fbd2
hanulkimm <hanulkim2000@naver.com>
2023-04-07 17:57:16 +0900

    update

--- Week 10/#3 욕심쟁이판다.py
+++ Week 10/#3 욕심쟁이판다.py
@@ -1,31 +1,24 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+## dfs + dp로 풀기
+## dfs 시간초과
 
-def dfs(i,j,cnt):
-    global tmp
-    flag = True
+def dfs(i,j):
+    if dp[i][j]:
+        return dp[i][j]
+    dp[i][j]=1
     for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
         ni,nj=i+di, j+dj
-        if 0<=ni<n and 0<=nj<n and v[ni][nj]==0 and arr[ni][nj]>arr[i][j]:
-            flag=False
-            v[ni][nj]=1
-            dfs(ni,nj,cnt+1)
-            v[ni][nj]=0
-    if flag:
-        tmp = max(tmp, cnt)
-        return
-
+        if 0<=ni<n and 0<=nj<n and arr[ni][nj]>arr[i][j]:
+            dp[i][j] = max(dp[i][j], dfs(ni,nj)+1)
+    return dp[i][j]
 
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 ans = 0
-v = [[0]*n for _ in range(n)]
+dp = [[0]*n for _ in range(n)]
 for i in range(n):
     for j in range(n):
-        tmp = 0
-        v[i][j]=1
-        dfs(i,  j, 1)
-        v[i][j]=0
-        ans = max(tmp, ans)
+        ans = max(ans, dfs(i,j))
 print(ans)
